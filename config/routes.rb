23e8f8de76cae67dbcfff07a551efedc5e7515c1
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-30 03:29:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
 
   get '/students/:id', to: 'students#show', as: 'student'
-  #post '/students', to: 'students#create'
+  post '/students', to: 'students#create'
   
  end
